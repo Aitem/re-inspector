b970c54d6ebe61fd91495475d974c37d98c4cbc7
suchimauz <alexey.fedulov98@gmail.com>
2019-08-14 17:01:56 +0300

    test portmessages

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
 chrome.runtime.onConnectExternal.addListener(function(port) {
     alert(port.name);
     });
+
+chrome.runtime.onMessageExternal.addListener(function(msg) {
+    alert("lol" + msg.data)
+});
